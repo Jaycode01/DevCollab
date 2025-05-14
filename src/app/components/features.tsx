9fedab36fd80a6cb6570ddf2f2d92a13323927c2
Jaycode01 <josephlamidi01@gmail.com>
2025-05-14 05:18:23 +0100

    worked on the featurees section
    the flex was a bit challenging but i think i killed it sh

--- src/app/components/features.tsx
+++ src/app/components/features.tsx
@@ -1,16 +1,85 @@
+import { didact_gothic, montserrat } from "./fonts";
+import Image from "next/image";
+import TeamCollabortion from "../../../public/team-collaboration.svg";
+import SmartDashboard from "../../../public/smart-dashboard.svg";
+import ProjectsOrganization from "../../../public/projects-organization.svg";
+import TaskManagement from "../../../public/task-management.svg";
+
 export default function Features() {
   return (
     <>
-      <div>
-        <h1>Collaborate and Build with DevCollab</h1>
-        <p>
-          Make your projects more organized and track progress on all your
-          projects with your team.
-        </p>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+      <div className=" mx-auto w-11/12 mt-20  ">
+        <div className="flex flex-col gap-2.5 ,d:w-1/3 w-full">
+          <h1 className={`${montserrat.className} font-bold text-3xl`}>
+            Collaborate and Build with DevCollab
+          </h1>
+          <p className={`${didact_gothic.className} font-bold text-lg `}>
+            Make your projects more organized and track progress on all your
+            projects with your team.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row itmes-center justify-between gap-5 md:gap-10 md:w-11/12  w-full mx-auto mt-9 ">
+          <div className="h-fit py-4  flex-1 aspect-square px-5 bg-gray-50 flex flex-col gap-7">
+            <Image
+              alt="Team Collaboration Image"
+              src={TeamCollabortion}
+              width={150}
+              height={150}
+            />
+            <h2 className="text-lg">Team Collaboration</h2>
+            <p className="text-[14px] md:text-sm">
+              Work better together, wherever you are. Collaborate in real-time
+              with task-based commenting, user mentions, and shared project
+              spaces. Empower your team to stay aligned, provide updates, and
+              contribute ideas — all within DevCollab.
+            </p>
+          </div>
+          <div className="h-fit py-4  flex-1 aspect-square px-5 bg-gray-50 flex flex-col gap-7">
+            <Image
+              src={SmartDashboard}
+              alt="Smart Dashboard feature image"
+              width={150}
+              height={150}
+            />
+            <h2 className="text-lg">Smart Dashboard</h2>
+            <p className="text-[14px] md:text-sm">
+              Make smarter decisions with real-time overviews. Your personalized
+              dashboard gives you instant visibility into your team workload,
+              upcoming deadlines, task status, and overall project health.
+              Understand whats happening — and what needs your attention.
+            </p>
+          </div>
+          <div className="h-fit py-4  flex-1 aspect-square px-5 bg-gray-50 flex flex-col gap-7">
+            <Image
+              src={ProjectsOrganization}
+              alt="Project organization feature image"
+              width={150}
+              height={150}
+            />
+            <h2 className="text-lg">Projects Organization</h2>
+            <p className="text-;14px] md:text-sm">
+              Structure your work with clarity. Group related tasks under
+              dedicated projects, set key milestones, and break down large goals
+              into manageable steps. With clean project dashboards, everything
+              your team needs stays in one place — visible and under control.
+            </p>
+          </div>
+          <div className="h-fit py-4  flex-1 aspect-square px-5 bg-gray-50 flex flex-col gap-7">
+            <Image
+              alt="Tasks management Feature image"
+              src={TaskManagement}
+              width={150}
+              height={150}
+            />
+            <h2 className="text-lg">Tasks Management</h2>
+            <p className="text-[14px] md:text-sm">
+              Keep your team organized and accountable with DevCollab’s
+              intuitive task manager. Easily create tasks, assign them to
+              teammates, set deadlines, and track progress from start to finish.
+              Use statuses, priorities, and labels to streamline your workflow —
+              whether you are managing bugs, features, or sprints.
+            </p>
+          </div>
         </div>
       </div>
     </>
