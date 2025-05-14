596eed1d881f067cf2b66ca97e62fe3cb050f4ca
Jaycode01 <josephlamidi01@gmail.com>
2025-05-14 23:13:08 +0100

    working on the section for userer to have different profile content when signed in and when not siged in

--- src/app/components/user-quick-box.tsx
+++ src/app/components/user-quick-box.tsx
@@ -1,14 +1,34 @@
-import Link from "next/link";
-import Image from "next/image";
-import cancelBox from "../../../public/cancel.svg";
+"use client";
 
-export default function UserQuickBox() {
+// import Link from "next/link";
+import { User } from "firebase/auth";
+import Image from "next/image";
+// import cancelBox from "../../../public/cancel.svg";
+
+interface Props {
+  user: User;
+}
+
+export default function UserQuickBox({ user }: Props) {
   return (
     <>
-      <div>
-        <div>
-          <Link href="/user-profile">view profile</Link>
-          <Image alt="Camcel Icon" src={cancelBox} width={20} height={20} />
+      <div className="absolute top-full right-0 mt-2 w-64 bg-white shadow-md rounded-md p-4 z-50">
+        <div className="flex items-center gap-3">
+          {user.photoURL && (
+            <Image
+              src={user.photoURL}
+              alt="User Avatar"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+          )}
+          <div className="flex flex-col text-sm">
+            <span className="font-semibold">
+              {user.displayName || "No Name"}
+            </span>
+            <span className="text-gray-700">{user.email}</span>
+          </div>
         </div>
       </div>
     </>
