cca199318ba322eb58c4e226ce2bed0ea9bd32f6
Jaycode01 <josephlamidi01@gmail.com>
2025-05-14 01:44:43 +0100

    i make the view of it on mobile pretty fitting and stunning
    Let's keep the grind

--- src/app/components/hero-section.tsx
+++ src/app/components/hero-section.tsx
@@ -8,27 +8,32 @@ import AngleRightIcon from "../../../public/angle-right.svg";
 export default function HeroSection() {
   return (
     <>
-      <div className="bg-gray-50 flex items-center justify-center py-14">
-        <div className="flex flex-col gap-2.5 w-2/5">
-          <h1 className={`${space_grotesk.className} text-5xl font-bold`}>
+      <div className="bg-gray-50 flex flex-col md:flex-row items-center justify-center py-4 md:py-14 gap-7 md:gap-0 px-3">
+        <div className="flex flex-col gap-2.5 md:w-2/5 w-full">
+          <h1
+            className={`${space_grotesk.className} text-4xl md:text-5xl font-bold`}
+          >
             Collaborate. Build. Deliver.
           </h1>
-          <h3 className="text-[22px]">
+          <h3 className="text-[16px] md:text-[22px]">
             A workspace built for developers who ship fast.
           </h3>
-          <h4>
+          <h4 className="text-[13px] md:text-[1rem]">
             DevCollab helps developers manage projects, tasks, and collaborate
             to bring project to life which will solve problems and help the
             dynamic environment grow — all in one place.
           </h4>
-          <div className="flex gap-8 items-center mt-5">
+          <div className="flex gap-5  md:gap-8 items-center mt-5">
             <button
               type="button"
-              className=" bg-blue-600 opacity-80 hover:opacity-100 text-white py-3 px-5"
+              className=" bg-blue-600 opacity-80 hover:opacity-100 text-white py-3 px-5 text-sm md:text-[1rem]"
             >
               Join Free
             </button>
-            <button type="button" className="group flex items-center">
+            <button
+              type="button"
+              className="group flex items-center text-sm md:text-[1rem]"
+            >
               Learn More
               <Image
                 alt="Right Angle"
@@ -39,7 +44,7 @@ export default function HeroSection() {
           </div>
           <Link
             href="https://i-dontknow.com"
-            className="group text-blue-600 flex items-center gap-2.5 capitalize text-lg mt-2.5"
+            className="group text-blue-600 flex items-center gap-2.5 capitalize md:text-lg mt-2.5 text-sm"
           >
             watch video{" "}
             <Image
@@ -49,7 +54,7 @@ export default function HeroSection() {
             />
           </Link>
         </div>
-        <div className="w-2/5">
+        <div className="md:w-2/5 w-full">
           <Image src={HeroImage} alt="Hero Image" className="w-full" />
         </div>
       </div>
