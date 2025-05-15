95990877c4455241a9e87ca6561c888a260dcd20
Jaycode01 <josephlamidi01@gmail.com>
2025-05-15 11:37:22 +0100

    worked on the footer section
    think i am abit done on the index page...will work on the aimation now

--- src/app/components/notification-quick-box.tsx
+++ src/app/components/notification-quick-box.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+
 export default function NotificationQuickBox() {
   return (
     <>
-      <div className="absolute bg-white w-72 rounded-sm left-[-250px] z-50 top-16 py-4 px-3 flex flex-col gap-5">
-        <h4 className="font-semi-bold mb-2">Notification</h4>
+      <div className="absolute bg-white w-72 shadow-md rounded-sm left-[-250px] z-50 top-16 py-4 px-3 flex flex-col gap-5">
+        <h4 className="font-semi-bold mb-2 flex flex-row items-center justify-between">
+          Notification{" "}
+          <Link
+            href="/dashboard"
+            className="text-[12px] underline hover:text-blue-600 text-gray-900"
+          >
+            see all
+          </Link>
+        </h4>
 
         <ul className="text-sm text-gray-700 flex flex-col gap-2.5">
           <li className="py-1 border-b cursor-pointer">
