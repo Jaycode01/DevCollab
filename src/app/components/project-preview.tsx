1cf24fcc937373bdb0edb5b685b7f02ae40b1d57
Jaycode01 <josephlamidi01@gmail.com>
2025-05-15 20:45:47 +0100

    full animation worked on

--- src/app/components/project-preview.tsx
+++ src/app/components/project-preview.tsx
@@ -1,10 +1,42 @@
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 import PreviewImage from "../../../public/images/fake-decollab-preview.png";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Preview() {
+  const previewScaling = useRef(null);
+
+  useEffect(() => {
+    if (!previewScaling.current) return;
+
+    gsap.fromTo(
+      previewScaling.current,
+      { scale: 0.5, opacity: 0 },
+      {
+        scrollTrigger: {
+          trigger: previewScaling.current,
+          start: "top 80%",
+          toggleActions: "play none none reverse",
+        },
+        scale: 1,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.out",
+      }
+    );
+  }, []);
+
   return (
     <div className="">
-      <div className="mt-14 bg-gray-50 w-11/12 mx-auto rounded-md">
+      <div
+        className="mt-14 bg-gray-50 w-11/12 mx-auto rounded-md"
+        ref={previewScaling}
+      >
         <Image
           alt="Preview Image"
           src={PreviewImage}
