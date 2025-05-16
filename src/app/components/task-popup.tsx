36ffa4521d6da89ec1776eeffd528750b0272f36
Jaycode01 <josephlamidi01@gmail.com>
2025-05-16 16:40:02 +0100

    worked on sorting tasks and also the popup when clicked on

---
+++ src/app/components/task-popup.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import CancelPopup from "../../../public/cancel.svg";
+
+export default function TaskPopup() {
+  return (
+    <div className="absolute top-1/4 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-xl p-5 rounded-sm h-fit flex flex-col justify-center  w-[300px] gap-5 border">
+      <div className="flex justify-between items-center">
+        <h3 className="text-[18px]">Project Name</h3>
+        <Image src={CancelPopup} alt="cancel popup" width={20} height={20} />
+      </div>
+      <div className="flex items-center justify-between">
+        <p className="text-sm text-gray-600">Description...</p>
+        <p className="text-[11px]">Tag</p>
+      </div>
+      <div className="flex gap-2 items-center">
+        <p className="border px-2 py-1">Figma</p>
+        <p className="border px-2 py-1">Next.js</p>
+        <p className="border px-2 py-1">Firebase</p>
+      </div>
+      <div className="text-gray-500 text-[12px]">
+        <p className="">Notes...</p>
+      </div>
+      <div className="flex items-center justify-between">
+        <p className="">Zach Latta</p>
+        <p className="">17/05</p>
+      </div>
+    </div>
+  );
+}
