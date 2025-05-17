5edaa910f94daefbf452161ea4ce7e8e8f36f878
Jaycode01 <josephlamidi01@gmail.com>
2025-05-17 02:25:10 +0100

    have to give up on this titirng backend again

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import type React from "react";
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
