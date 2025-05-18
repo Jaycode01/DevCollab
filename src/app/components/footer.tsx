bd135f8970be794dc499a10ac1080255c7b50ae6
Jaycode01 <josephlamidi01@gmail.com>
2025-05-18 20:19:42 +0100

    Update footer layout and adjust link text size for better readability

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-500 py-5 flex justify-center items-start text-sm text-white w-full md:gap-[5%] gap-7 px-3">
-      <div className="flex flex-col gap-4 w-1/4">
+    <div className="bg-gray-500 py-5 flex flex-col md:flex-row justify-center items-start text-sm text-white w-full md:gap-[5%] gap-7 px-3">
+      <div className="flex flex-col gap-4 md:w-1/4 w-full">
         <Image
           alt="decollab logo"
           src={Logo}
@@ -21,28 +21,52 @@ export default function Footer() {
       <div className="flex flex-col gap-4">
         <h3 className="text-gray-900 font-bold md:text-lg">Quick Links</h3>
         <div className="flex flex-col gap-1.5">
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
+          <Link className=" text-[12px] md:text-sm" href="/about">
+            About
+          </Link>
+          <Link className=" text-[12px] md:text-sm" href="/contact">
+            Contact
+          </Link>
         </div>
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-gray-900 md:text-lg font-bold">Resources</h3>
         <div className="flex flex-col gap-1.5">
-          <Link href="">Community</Link>
-          <Link href="">Developers</Link>
-          <Link href="">Guides</Link>
+          <Link className=" text-[12px] md:text-sm" href="">
+            Community
+          </Link>
+          <Link className=" text-[12px] md:text-sm" href="">
+            Developers
+          </Link>
+          <Link className=" text-[12px] md:text-sm" href="">
+            Guides
+          </Link>
         </div>
       </div>
       <div className="flex flex-col gap-4">
         <h3 className="text-gray-900 md:text-lg font-bold">Socials</h3>
         <div className="flex flex-col gap-1.5">
-          <Link href="">Twitter(X)</Link>
-          <Link href="">LinkedIn</Link>
-          <Link href="">GitHub</Link>
-          <Link href="">YouTube</Link>
-          <Link href="">Thread</Link>
-          <Link href="">Instagram</Link>
-          <Link href="">TikTok</Link>
+          <Link className=" text-[12px] md:text-sm" href="">
+            Twitter(X)
+          </Link>
+          <Link className=" text-[12px] md:text-sm" href="">
+            LinkedIn
+          </Link>
+          <Link className="text-[12px] md:text-sm" href="">
+            GitHub
+          </Link>
+          <Link className="text-[12px] md:text-sm" href="">
+            YouTube
+          </Link>
+          <Link className="text-[12px] md:text-sm" href="">
+            Thread
+          </Link>
+          <Link className="text-[12px] md:text-sm" href="">
+            Instagram
+          </Link>
+          <Link className="text-[12px] md:text-sm" href="">
+            TikTok
+          </Link>
         </div>
       </div>
     </div>
