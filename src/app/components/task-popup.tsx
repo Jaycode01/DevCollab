e11786019018658a9095d23e229c84f00116b9ee
Jaycode01 <josephlamidi01@gmail.com>
2025-05-18 16:27:25 +0100

    Enhance project summary and task popup with modal functionality and project details display

--- src/app/components/task-popup.tsx
+++ src/app/components/task-popup.tsx
@@ -3,37 +3,35 @@
 import Image from "next/image";
 import CancelPopup from "../../../public/cancel.svg";
 
+type Project = {
+  id: number;
+  name: string;
+  update: string;
+  due: string;
+  status: string;
+};
+
 type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
-  children: React.ReactNode;
+  project: Project | null;
 };
 
-export default function TaskPopup({ isOpen, onClose }: ModalProps) {
-  if (!isOpen) return null;
+export default function TaskPopup({ isOpen, onClose, project }: ModalProps) {
+  if (!isOpen || !project) return null;
+
   return (
-    <div className="fixed top-1/2  md:top-1/4 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-xl p-5 rounded-sm h-fit flex flex-col justify-center  w-[300px] gap-5 border">
-      <div className="flex justify-between items-center">
-        <h3 className="text-[18px]">Project Name</h3>
-        <button onClick={onClose} type="button">
+    <div className="fixed top-1/2 md:top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-xl p-5 rounded-sm w-[300px] gap-5 border z-50">
+      <div className="flex justify-between items-center mb-4">
+        <h3 className="text-[18px] font-semibold">{project.name}</h3>
+        <button onClick={onClose} type="button" aria-label="Close modal">
           <Image src={CancelPopup} alt="cancel popup" width={20} height={20} />
         </button>
       </div>
-      <div className="flex items-center justify-between">
-        <p className="text-sm text-gray-600">Description...</p>
-        <p className="text-[11px]">Tag</p>
-      </div>
-      <div className="flex gap-2 items-center">
-        <p className="border px-2 py-1">Figma</p>
-        <p className="border px-2 py-1">Next.js</p>
-        <p className="border px-2 py-1">Firebase</p>
-      </div>
-      <div className="text-gray-500 text-[12px]">
-        <p className="">Notes...</p>
-      </div>
-      <div className="flex items-center justify-between">
-        <p className="">Zach Latta</p>
-        <p className="">17/05</p>
+      <div className="mb-3">
+        <p className="text-sm text-gray-600">Last Update: {project.update}</p>
+        <p className="text-sm text-gray-600">Due Date: {project.due}</p>
+        <p className="text-sm text-gray-600">Status: {project.status}</p>
       </div>
     </div>
   );
