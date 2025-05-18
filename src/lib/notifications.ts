a64b69e3581eacb0895e745b8c04c420faeafadc
Jaycode01 <josephlamidi01@gmail.com>
2025-05-18 15:58:24 +0100

    Refactor notification handling by moving notifications to a shared module

---
+++ src/lib/notifications.ts
@@ -0,0 +1,10 @@
+export const notifications = [
+  { id: 1, message: "New team member added" },
+  { id: 2, message: "You just committed to X project" },
+  { id: 3, message: "Peter Pan reviewed your code" },
+  { id: 4, message: "Meeting scheduled" },
+  { id: 5, message: "New comment" },
+  { id: 6, message: "Code deployed" },
+  { id: 7, message: "Production bug resolved" },
+  { id: 8, message: "New task assigned" },
+];
