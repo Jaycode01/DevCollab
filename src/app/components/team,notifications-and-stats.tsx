fcd94e0eb67bda3951a460347238a229d2527b81
Jaycode01 <josephlamidi01@gmail.com>
2025-05-18 15:49:58 +0100

    Refactor notification components and implement notification data management

--- src/app/components/team,notifications-and-stats.tsx
+++ src/app/components/team,notifications-and-stats.tsx
@@ -1,12 +1,12 @@
 import Calendar from "./calendar";
-import Notification from "./notification";
+import NotificationData from "./notificationData";
 import Team from "./team";
 
 export default function TeamAndNotifications() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 mt-2 gap-4 px-4 justify-between">
       <Team />
-      <Notification />
+      <NotificationData />
       <Calendar />
     </div>
   );
