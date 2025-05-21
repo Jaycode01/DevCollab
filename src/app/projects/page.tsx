4968353db22fa193b227210bb1ee73501ccdde4c
Jaycode01 <josephlamidi01@gmail.com>
2025-05-21 01:45:15 +0100

    Add project display enhancements and collaborator images in Projects component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,16 @@
 import Search from "../../../public/search.svg";
 import AddIcon from "../../../public/add.svg";
 import ProjectTestImage from "../../../public/square-3-stack.svg";
+import Avatar from "../../../public/images/fakeUserPic.png";
 import Link from "next/link";
+import Dots from "../../../public/dots.svg";
 import Image from "next/image";
+import { projects } from "../../lib/projectsData";
 
 export default function Projects() {
   return (
-    <div className="w-full">
-      <div className="mt-5 flex md:flex-row flex-col justify-between items-center border-b-2 border-gray-900 py-3 w-full md:px-5 px-2 gap-3.5 md:gap-0">
+    <div className="w-full bg-gray-50 pb-5">
+      <div className="mt-5 flex md:flex-row flex-col justify-between items-center border-b-2 border-gray-900 py-3 w-full md:px-5 px-2 gap-3.5 md:gap-0 bg-white">
         <div className="md:w-3/5 w-full flex flex-row md:gap-3 gap-2 items-center">
           <input
             type="search"
@@ -38,19 +41,76 @@ export default function Projects() {
           </button>
         </div>
       </div>
-      <div className="">
-        <div className="">
-          <div className="">
-            <Image src={ProjectTestImage} alt="project image" />
-            <div className="">
-              <p className="">Project Name</p>
-              <Link href="google.com">https://google.com/</Link>
+      <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-5">
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className=" bg-white border border-gray-900 rounded-md p-4 shadow-lg flex flex-col gap-3 cursor-pointer hover:scale-98 transition-all duration-300 ease-in-out"
+          >
+            <div className="flex justify-between items-center">
+              <div className="flex flex-row items-center gap-2">
+                <Image
+                  src={ProjectTestImage}
+                  alt="project image"
+                  width={60}
+                  height={60}
+                  className="border-2 border-gray-900 rounded-md p-2"
+                />
+                <div className="">
+                  <p className="text-[17px]">{project.name}</p>
+                  <Link
+                    href={project.url}
+                    target="_blank"
+                    className="text-[14px] text-blue-600 hover:border-b  border-blue-600"
+                  >
+                    {project.url}
+                  </Link>
+                </div>
+              </div>
+              <button type="button" className="">
+                <Image src={Dots} alt="project action icon" />
+              </button>
             </div>
+            <div className="flex relative">
+              <Image
+                src={Avatar}
+                alt="collaborator image"
+                width={30}
+                height={30}
+                className="absolute left-0 top-0 border border-gray-900 rounded-full"
+              />
+              <Image
+                src={Avatar}
+                alt="collaborator image"
+                width={30}
+                height={30}
+                className="absolute left-5 top-0 border border-gray-900 rounded-full"
+              />
+              <Image
+                src={Avatar}
+                alt="collaborator image"
+                width={30}
+                height={30}
+                className="absolute left-10 top-0 border border-gray-900 rounded-full"
+              />
+              <Image
+                src={Avatar}
+                alt="collaborator image"
+                width={30}
+                height={30}
+                className="absolute left-15 top-0 border border-gray-900 rounded-full"
+              />
+              <Image
+                src={Avatar}
+                alt="collaborator image"
+                width={30}
+                height={30}
+                className="absolute left-20 top-0 border border-gray-900 rounded-full"
+              />
+            </div>
+            <p className="mt-6 text-sm text-gray-900">{project.updatedAt}</p>
           </div>
-          <div className="">
-            <Image />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
