d2c8a6ee0e01fcfedbd4f8ee920e9ddee3a69bbd
Jaycode01 <josephlamidi01@gmail.com>
2025-05-29 18:13:21 +0100

    refactor: remove NotificationData component from TeamAndNotifications

--- frontend/src/app/components/team,notifications-and-stats.tsx
+++ frontend/src/app/components/team,notifications-and-stats.tsx
@@ -1,12 +1,10 @@
 import Calendar from "./calendar";
-import NotificationData from "./notificationData";
 import Team from "./team";
 
 export default function TeamAndNotifications() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 mt-2 gap-4 px-4 justify-between">
       <Team />
-      <NotificationData />
       <Calendar />
     </div>
   );
