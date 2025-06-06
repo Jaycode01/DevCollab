d1faedd836e71170b6d6490a14ad70efe6c34115
Jaycode01 <josephlamidi01@gmail.com>
2025-06-06 19:33:34 +0100

    updated the useUsageTracker.ts ti avoid unnecccessary API calss when tab is inactive or hidden.

--- frontend/src/hooks/useUsageTracker.ts
+++ frontend/src/hooks/useUsageTracker.ts
@@ -1,12 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import axios from "axios";
 
 export default function useUsageTracker(
   token: string | null,
   loading: boolean
 ) {
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
   useEffect(() => {
     if (loading || !token) {
       console.log("🔃 Still loading or no token yet.", { loading, token });
@@ -16,6 +17,11 @@ export default function useUsageTracker(
     const API_BASE = process.env.NEXT_PUBLIC_API_URL;
     const sendUsage = async () => {
       try {
+        if (document.hidden) {
+          console.log("Tab not visible, skipping usage log");
+          return;
+        }
+
         console.log("📡 Sending usage log...");
         await axios.post(
           `${API_BASE}/api/logged-hours`,
@@ -32,8 +38,39 @@ export default function useUsageTracker(
       }
     };
 
-    sendUsage();
-    const intervalId = setInterval(sendUsage, 60 * 1000);
-    return () => clearInterval(intervalId);
+    const startTracking = () => {
+      if (!intervalRef.current) {
+        sendUsage();
+        intervalRef.current = setInterval(sendUsage, 10 * 60 * 1000);
+        console.log("Tracking started");
+      }
+    };
+
+    const stopTracking = () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+        console.log("Tracking stopped.");
+      }
+    };
+
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === "visible") {
+        startTracking();
+      } else {
+        startTracking();
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    if (document.visibilityState === "visible") {
+      startTracking();
+    }
+
+    return () => {
+      stopTracking();
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
   }, [token, loading]);
 }
