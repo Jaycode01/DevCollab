bcd360b503bb39323651e04d70a7eaa633636e2f
Jaycode01 <josephlamidi01@gmail.com>
2025-06-11 00:02:06 +0100

    getting started with teams backend codes

---
+++ backend/routes/teams.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { FieldValue } from "firebase-admin/firestore";
+import { db } from "../firebase";
+const router = express.Router();
+const db = admin.firestore();
+
+router.post("/teams", async (req, res) => {
+  try {
+    const { name, description, userUid } = req.body;
+
+    if (!name || !userUid) {
+      return res.status(400).json({ message: "Missing required fields." });
+    }
+
+    const teamRef = db.collection("teams").doc();
+
+    await teamRef.set({
+      name,
+      description: description || "",
+      createdBy: userUid,
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      members: [{ uid: userUid, role: admin }],
+      memberUids: [userUid], //Makes it easy to query later
+    });
+
+    res.status(201).json({ message: "Team created", teamId: teamRef.id });
+  } catch (error) {
+    console.error("Error creating team:", error);
+    res.status(500).json({ message: "Server error." });
+  }
+});
+
+module.exports = router;
