3a7deb708c96a9dd0221472757f8524c7682bbc1
Jaycode01 <josephlamidi01@gmail.com>
2025-06-14 23:07:12 +0100

    still trying to figure out the problem rn

--- frontend/src/app/team/page.tsx
+++ frontend/src/app/team/page.tsx
@@ -48,7 +48,7 @@ export default function Team() {
 
     if (!userUid) return;
 
-    const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+    const API_BASE = process.env.NEXT_PUBLIC_API_URL || "localhost:5000";
 
     try {
       const res = await fetch(`${API_BASE}/api/teams?userUid=${userUid}`);
