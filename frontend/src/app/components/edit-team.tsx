8bc5b55ccc9f7947158e56e6ea2457ba3c4dd4b4
Jaycode01 <josephlamidi01@gmail.com>
2025-06-16 02:08:13 +0100

    i have to use the app in the inde.js
    I think that is where the issue is coming from

--- frontend/src/app/components/edit-team.tsx
+++ frontend/src/app/components/edit-team.tsx
@@ -36,7 +36,8 @@ export default function EditTeam({
     setloading(true);
 
     try {
-      const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+      const API_BASE =
+        process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
       const res = await fetch(`${API_BASE}/api/teams/${team.id}/edit`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
