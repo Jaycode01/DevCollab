3e535f69319a46ec4577e50aa3d66ef61fd64deb
Jaycode01 <josephlamidi01@gmail.com>
2025-07-04 00:00:23 +0100

    started working on contact page

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,26 +1,44 @@
+import { Mail, User } from "lucide-react";
+
 export default function ContactUs() {
   return (
-    <div>
-      <div className="flex flex-col justify-center h-[100vh] pl-[20%]">
-        <div className="bg-blue-200 w-fit p-3">
-          <p className="text-left">
-            Note: Your Infos will be sent with the message for good user(your)
-            experience
-          </p>
+    <div className="flex flex-col h-[100vh] bg-white justify-center items-center">
+      <form
+        action=""
+        className="w-[55%] border-gray-600 border p-7 rounded flex flex-col gap-5"
+      >
+        <div className="border border-gray-600 rounded p-3 flex items-center gap-1.5">
+          <label htmlFor="name" className="border-r-[2px] border-gray-600 pr-2">
+            <User />
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="border-none outline-none w-full text-inherit px-2.5"
+            placeholder="Nex0n"
+          />
         </div>
-        <form action="" className="w-[70%]">
-          <div className="flex flex-col gap-1">
-            <label htmlFor="">Message:</label>
-            <textarea className="border border-gray-900 px-4 py-2 h-[200px] outline-none text-sm" />
-          </div>
-          <button
-            type="submit"
-            className="w-full text-center text-white bg-blue-600 py-2 mt-5 hover:bg-blue-500"
+        <div className="border border-gray-600 rounded p-3 flex items-center gap-1.5">
+          <label
+            htmlFor="email"
+            className="border-r-[2px] border-gray-600 pr-2"
           >
-            Send
-          </button>
-        </form>
-      </div>
+            <Mail />
+          </label>
+          <input
+            type="email"
+            className="outline-none w-full text-inherit px-2.5"
+            placeholder="nexon@dev.com"
+          />
+        </div>
+        <textarea
+          name=""
+          id=""
+          className="border border-gray-600 rounded h-[200px] p-3"
+          placeholder="Hi Devcollab......"
+        />
+      </form>
     </div>
   );
 }
